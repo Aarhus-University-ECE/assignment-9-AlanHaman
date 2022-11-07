7779a390e29a8dd88ebad4c0e97e74d7d1d460b5
AlanHaman <114674762+AlanHaman@users.noreply.github.com>
2022-11-07 21:15:12 +0100

    Update sum.c

--- src/sum.c
+++ src/sum.c
@@ -1,6 +1,11 @@
 #include "sum.h"
 
-int sum(int a[], int n)
-{
-    return 0;
+int sum(int a[], int n) {
+    // if n we is equal 0, we return n, which is 0
+    if (n == 0) {
+        return n;
+        // if n is not 0, we return the value of the element we are at plus the sum of the array from the element to the bottom.
+    } else {
+        return a[n - 1] + sum(a, n - 1);
+    }
 }
